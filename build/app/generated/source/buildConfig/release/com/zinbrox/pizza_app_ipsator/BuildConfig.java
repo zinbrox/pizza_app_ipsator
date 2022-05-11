81df9f75bc9732bfab5c1b6d8061bb59690c0a55
zinbrox <53508807+zinbrox@users.noreply.github.com>
2022-05-12 00:12:40 +0530

    Generated apk file

---
+++ build/app/generated/source/buildConfig/release/com/zinbrox/pizza_app_ipsator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zinbrox.pizza_app_ipsator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.zinbrox.pizza_app_ipsator";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
